6341f950d361a4d49b50d4f8f7247898f9f65986
BuAleksandr <87057144+BuAleksandr@users.noreply.github.com>
2021-10-18 21:52:06 +1100

    ProPY_lesson_3

---
+++ Web-scrapping.py
@@ -0,0 +1,21 @@
+import requests
+import bs4
+
+KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'Удалённая работа']
+KEYWORDS_set = set(KEYWORDS)
+if __name__ == '__main__':
+    response = requests.get('https://habr.com/ru/all/')
+    response.raise_for_status()
+    soup = bs4.BeautifulSoup(response.text, features='html.parser')
+    articles = soup.find_all('article')
+    for article in articles:
+        hubs = article.find_all(class_='tm-article-snippet__hubs-item')
+        hubs = set(hub.find('span').text for hub in hubs)
+        if KEYWORDS_set & hubs:
+            href = article.find(class_='tm-article-snippet__title-link').attrs['href']
+            link = "https://habr.com" + href
+            date = article.find(class_='tm-article-snippet__datetime-published')
+            print(date.text, '-', article.find('h2').text, '-', link)
+
+
+
